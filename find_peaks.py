eb1f73fc6ffdd29019e2d6247bdc3e3a9c74d872
Aaron Wolfe <wolfe.aarond@gmail.com>
2021-06-24 17:00:02 -0400

    added bresenham line formula

--- find_peaks.py
+++ find_peaks.py
@@ -1,13 +1,3 @@
-from operator import itemgetter
-from PIL import Image
-import numpy as np
-from numpy.lib import imag
-from scipy import ndimage
-import matplotlib.pyplot as plt
-from findpeaks import findpeaks
-from tqdm import tqdm
-
-
 # THIS IS FUNCTIONAL!
 # https://www.sthu.org/code/codesnippets/imagepers.html
 # Uses topology to determine peaks. 
@@ -16,59 +6,131 @@ from tqdm import tqdm
 
 # I Think I need to write custom version that can take into account no peaks WITHIN other peaks for prominence
 
+# UPDATE 6-24-2021: Tried using a median filter around each peak to see if I can filter out non-peak like peaks from the topology algo. 
+# Unfortunately, I wasn't able to find a window that could distinguish. May be other methods
+
+# Next attempt: create a minimum spanning tree, and find the center point of each line segment. The 
+# average value of these is likely to be a good representation of the "cell background". 
+# 1. Calculate a minimum spanning tree
+# 2. Calculate a mean for all points in tree, and "centerpoint mean" for each edge
+# 2. If a peak is closer to the mean than it is to the centerpoint mean, then the peak is barely a peak. 
+
+from operator import itemgetter
+from PIL import Image
+import numpy as np
+from numpy.lib import imag
+from scipy import ndimage
+import matplotlib.pyplot as plt
+from findpeaks import findpeaks
+from tqdm import tqdm
+import networkx as nx
+
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __sub__(self, other):
+        return (self.x - other.x)**2 + (self.y - other.y)**2
+        # No need to square root to compare distances.
+
 
 def label_peaks(data):
+    """ 
+    This is the main peak labeling function. Uses the findpeaks library to determine significant peaks
+    in an image. The algorithm works REALLY well at labeling peaks when phase separated, but does not 
+    filter out the diffuse frames at all. 
 
+    Input: raw image data (single frame)
+    Output: a list of slices that contain the topological peaks. 
+    """
 
-
-    # Gauss filter to smooth out the clipped peaks
-    gauss = ndimage.filters.gaussian_filter(data,sigma=1) # 0.75 + window 4 less conservative
+    
 
     # Use findpeaks to label the peaks via topological prominence
-    fp = findpeaks(method='topology', window=4)
-    results = fp.fit(gauss)
-    #print(results['persistence'])
+    fp = findpeaks(method='topology', window=3)
+    results = fp.fit(data)
 
     # Create an array of the peaks labeled for numpy interaction
     maxima = np.array(results['Xdetect'])
-    unique, counts = np.unique(maxima, return_counts=True)
+    #unique, counts = np.unique(maxima, return_counts=True)
     labeled, num_objects = ndimage.label(maxima)
 
     # Slice the array to get the coordinates of the labeled pixels. 
     sliced = {i:list(zip(*np.where(labeled==i))) for i in np.unique(labeled) if i}
+    return sliced
+    
+def filter_peaks(slices):
+    """ Reads in the peaks, and determines whether a peak is significant enough (non-phase separated maxima still register as peaks) """
     xs, ys = [], []
-    neighs = []
-    median = ndimage.median_filter(data,size=2)
-    for i,coords in enumerate(sliced.values()):
+    
+    for i,coords in enumerate(slices.values()):
         for y,x in coords:
-            print(f"{i}: Raw {gauss[y][x]} vs median {median[y][x]} (percent: {int(data[y][x]/median[y][x]*100)}%)")
-            if int(data[y][x]/median[y][x]*100) > 120:
+            # Arbitrary filtration of peaks that are not above background
+            if gauss[y][x] > 200:
+
                 xs.append(x)
                 ys.append(y)
+    bresenham_line(xs[0],ys[0],xs[1],ys[1])
+    return xs,ys
 
+def bresenham_line(x0, y0, x1, y1):
+    # https://stackoverflow.com/questions/50995499/generating-pixel-values-of-line-connecting-2-points
+    steep = abs(y1 - y0) > abs(x1 - x0)
+    if steep:
+        x0, y0 = y0, x0  
+        x1, y1 = y1, x1
 
-    # New stuff:
-    # at each label, generate an median pixel value in a window
-    
-    return gauss,xs,ys,median
+    switched = False
+    if x0 > x1:
+        switched = True
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
 
-img = Image.open('data/test_data-14bit.tif')
+    if y0 < y1: 
+        ystep = 1
+    else:
+        ystep = -1
+
+    deltax = x1 - x0
+    deltay = abs(y1 - y0)
+    error = -deltax / 2
+    y = y0
+
+    line = []    
+    for x in range(x0, x1 + 1):
+        if steep:
+            line.append((y,x))
+        else:
+            line.append((x,y))
+
+        error = error + deltay
+        if error > 0:
+            y = y + ystep
+            error = error - deltax
+    if switched:
+        line.reverse()
+    print(f"From ({y0},{x0}) to ({y1},{x1}) is {line}")
+    return line
+
+#img = Image.open('data/test_data-14bit.tif')
+img = Image.open('data/FullScale_BGsub.tif')
 nframes = range(img.n_frames)[12:13]
-fig, axes = plt.subplots(len(nframes), 3, sharex=True, sharey=True)
 cycle = 0
 for i,frame in tqdm(enumerate(nframes)):
-    ax = axes.ravel()
     img.seek(frame)
     data = np.array(img)
-    gauss,xs,ys,median = label_peaks(data)
+    # Gauss filter to smooth out the clipped peaks
+    gauss = ndimage.filters.gaussian_filter(data,sigma=1)
+    slices = label_peaks(gauss)
+    xs,ys = filter_peaks(slices)
     
-    ax[cycle].imshow(data)                # 0,
+    fig, axes = plt.subplots(1, 2, sharex=True, sharey=True)
+    ax = axes.ravel()
+    ax[cycle].imshow(data)
     ax[cycle].axis('off')
     cycle+=1
-    ax[cycle].imshow(median)
-    cycle+=1
-    #ax[0].set_title('Original')
-    ax[cycle].imshow(gauss)               # 2,3
+    ax[cycle].imshow(data)
     ax[cycle].axis('off')
     ax[cycle].plot(xs,ys, 'r.')
     #for j in range(len(xs)):
@@ -76,5 +138,7 @@ for i,frame in tqdm(enumerate(nframes)):
     cycle+=1
     plt.autoscale(False)
     plt.savefig(f'result-{i}.png', bbox_inches = 'tight')
+    cycle=0
+    plt.show()
+    plt.close(fig)
 
-plt.show()
